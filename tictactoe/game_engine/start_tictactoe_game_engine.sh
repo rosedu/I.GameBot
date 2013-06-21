fad551252b5a72b0f6204bb44d5bf81ff0113ce7
Dan Serban <dserban01@yahoo.com>
2013-06-21 21:34:01 +0300

    s/python2/python/

--- tictactoe/game_engine/start_tictactoe_game_engine.sh
+++ tictactoe/game_engine/start_tictactoe_game_engine.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python2 src/start_game_engine.py /tmp/output_of_game_engine_input_of_player_1 /tmp/output_of_player_1_input_of_game_engine /tmp/output_of_game_engine_input_of_player_2 /tmp/output_of_player_2_input_of_game_engine player1 player2 2>/tmp/tictactoe_debug_log.txt
+python src/start_game_engine.py /tmp/output_of_game_engine_input_of_player_1 /tmp/output_of_player_1_input_of_game_engine /tmp/output_of_game_engine_input_of_player_2 /tmp/output_of_player_2_input_of_game_engine player1 player2 2>/tmp/tictactoe_debug_log.txt
 
