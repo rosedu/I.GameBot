9470bed1a0cbdbb4e9a675637c0ec05420447d9b
Razvan <razvan_gir@yahoo.com>
2014-03-16 07:50:09 -0700

    Added play_turn.py

---
+++ tomek/game_bots/random_bot_02_python/tomek_play_turn.py
@@ -0,0 +1,14 @@
+import random
+
+BOARD = ['a1', 'a2', 'a3', 'a4', 'b1', 'b2', 'b3', 'b4', 'c1', 'c2', 'c3', 'c4', 'd1', 'd2', 'd3', 'd4']
+
+def play_turn(
+        player_role,
+        owned_by_x,
+        owned_by_zero,
+        owned_by_t
+    ):
+
+    available_squares = list( set(BOARD) - set(owned_by_x) - set(owned_by_zero) -owned_by_t)
+
+    return random.choice(available_squares)
