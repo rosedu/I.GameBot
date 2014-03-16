d2283f93c28aa6322618003838b47939a70fe9d8
Razvan <razvan_gir@yahoo.com>
2014-03-16 08:19:32 -0700

    Made changes to scripts

--- tomek/game_bots/random_bot_02_python/run_tomek_game_bot_as_player_2.sh
+++ tomek/game_bots/random_bot_02_python/run_tomek_game_bot_as_player_2.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python tictactoe_game_bot.py /tmp/output_of_game_engine_input_of_player_2 /tmp/output_of_player_2_input_of_game_engine
+python tomek_game_bot.py /tmp/output_of_game_engine_input_of_player_2 /tmp/output_of_player_2_input_of_game_engine
 
