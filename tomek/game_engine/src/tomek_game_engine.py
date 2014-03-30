0778f12ec0d0ba31725a19c17f9241e69a37e9f0
cristi-zmf <root@cristizmf-Lenovo-G500.(none)>
2014-03-30 16:09:15 +0300

    Changed game_state so it has T

--- tomek/game_engine/src/tomek_game_engine.py
+++ tomek/game_engine/src/tomek_game_engine.py
@@ -65,8 +65,9 @@ def compute_points_scored(
     ]
 
     if turn >= 4:
-
+        
         empty_set = set([])
+        
         set_with_only_token_placed = set([token_placed])
         set_owned_by_x    = set(game_state['owned_by_x'])
         set_owned_by_zero = set(game_state['owned_by_zero'])
@@ -233,7 +234,7 @@ def start_game():
         game_state = {
             'owned_by_x':    [],
             'owned_by_zero': [],
-            'occupied_by_t': 'a1'   
+            'occupied_by_t': occupied_by_t   
         }
         progress_log['occupied_by_t'] = game_state['occupied_by_t']
         last_token_placed = ''
